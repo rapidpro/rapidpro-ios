c60325c6af288ee1b033dca61e15ccde9984c676
Daniel <daniel@ilhasoft.com.br>
2016-06-24 12:30:46 -0300

    Update U-Report icons

--- ureport/Classes/Firebase/URFireBaseManager.swift
+++ ureport/Classes/Firebase/URFireBaseManager.swift
@@ -15,9 +15,10 @@ class URFireBaseManager: NSObject {
 //    static let Path = "https://u-report.firebaseio.com/"
 //    static let GCM_DEBUG_MODE = true
     
-    static let GCM_DEBUG_MODE = true
-    static let Path = "https://u-report-dev.firebaseio.com/"
-    static let Properties = "Key-debug"
+    static let GCM_DEBUG_MODE = false
+//    static let Path = "https://u-report-dev.firebaseio.com/"
+    static let Path = "https://u-report-beta.firebaseio.com"
+    static let Properties = "Key"
     
     static let Reference = Firebase(url: Path)
     
